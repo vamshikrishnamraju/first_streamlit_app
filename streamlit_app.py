2338a0df1e45cb0fe656346009c5a54b422396b8
Vamshi Krishnam Raju <vamshi14krishna@gmail.com>
2023-05-03 17:20:15 +0530

    Update streamlit_app.py
    Updated the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("My parents healthy new diner")
+streamlit.header("Breakfast menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range egg")
